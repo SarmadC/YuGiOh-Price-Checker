78e14ef26ed13db179df47e35ed8cb75b87355cf
SarmadC <sarmadc45@gmail.com>
2022-11-07 18:57:39 -0700

    Add files via upload

---
+++ YGO Prices.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+import time
+import openpyxl
+
+df = pd.read_excel("Christian Gerlach.xlsx", usecols=['print_tag'])  # just dummy data, in your case this is
+
+
+# pd.read_excel
+
+def get_tag(print_tag):
+    request = requests.get(
+        'http://yugiohprices.com/api/price_for_print_tag/' + print_tag)
+    time.sleep(1)  # sleep for a second to prevent sending too many API calls per minute
+    return request.json()
+
+
+df['result'] = df['print_tag'].apply(get_tag)
+
+df2 = pd.json_normalize(df['result'])
+df2.to_excel('Christian YGO Analysis.xlsx')
