1d1680cf28099ad164161a8011a41e6df023a93d
SarmadC <sarmadc45@gmail.com>
2022-11-07 19:01:33 -0700

    Update YGO Prices.py

--- YGO Prices.py
+++ YGO Prices.py
@@ -5,7 +5,6 @@ import openpyxl
 
 df = pd.read_excel("Your Excel file with print tags.xlsx", usecols=['print_tag'])  
 
-
 # pd.read_excel
 
 def get_tag(print_tag):
@@ -13,7 +12,6 @@ def get_tag(print_tag):
         'http://yugiohprices.com/api/price_for_print_tag/' + print_tag)
     time.sleep(1)  # sleep for a second to prevent sending too many API calls per minute
     return request.json()
-
 
 df['result'] = df['print_tag'].apply(get_tag)
 
